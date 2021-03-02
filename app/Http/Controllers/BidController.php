423c181f06b6b3486d3073946e15333c14cd566b
njoroge33 <johngichuhi769@gmailcom>
2021-03-02 12:31:02 +0300

    rearrange transaction history

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -16,6 +16,7 @@ class BidController extends Controller
         $user_id = auth()->user()->profile_id;
         $bids = AuctionBid::with('auction')->where('profile_id', $user_id)->get();
         $transactions = Transaction::get();
+        $transactions = $transactions->sortByDesc('created_at');
         // $items= Item::get();
         // dd($bids);
         return view('bids', ['bids' => $bids, 'transactions' => $transactions]);
