17d93cdbb5c69ea5a9198bbbe166d63aea61d002
njoroge33 <johngichuhi769@gmailcom>
2021-03-02 10:09:06 +0300

    update account page

--- public/scripts/my.js
+++ public/scripts/my.js
@@ -1,19 +1,5 @@
 
 $(document).ready(function () {
-    $("#bid").click(function () {
-        $('#transaction').removeClass('active');
-        $('#bid').addClass('active');
-        $(".transaction").hide();
-        $(".bid").show();
-    });
-    
-    $("#transaction").click(function () {
-        $('#bid').removeClass('active');
-        $('#transaction').addClass('active');
-        $(".bid").hide();
-        $(".transaction").show();
-    });
-
     $("#item1").click(function () {
         // $('#bid').removeClass('active');
         // $('#transaction').addClass('active');
