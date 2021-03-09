ceb9d5e4b666d8d3598d8bb6909058c53e867e39
njoroge33 <johngichuhi769@gmailcom>
2021-03-09 17:16:31 +0300

    change the message sending functionality to a helper

--- app/Http/Controllers/Auth/PinController.php
+++ app/Http/Controllers/Auth/PinController.php
@@ -15,11 +15,7 @@ class PinController extends Controller
     }
 
     public function store(Request $request) {
-        $sms=["OTP_URL" => 'https://pyris.socialcom.co.ke/api/PushSMS.php?', 
-        "OTP_USERNAME" => 'sub_api_user', 
-        "OTP_PASSWORD" => '73wjzRNPAzgT8EZ1JQv8hpD2BsQ9', 
-        "OTP_SHORTCODE" => 'SOCIALCOM'];
-
+       
         $this->validate($request, [
             'phone'=> 'required|regex:/(07)[0-9]{8}/',
         ]);
@@ -33,24 +29,8 @@ class PinController extends Controller
             $new_pin = mt_rand(1000, 9999);
             $message = str_replace('<PIN>', $new_pin, $template->template_text);
 
-            $curl = curl_init();
+            $response = Utils::SendMessage($mobilenumber, $message);
             
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json',));
-
-            # send request to send otp via sms
-            $params = [
-                'username' => $sms['OTP_USERNAME'], 
-                'password' => $sms['OTP_PASSWORD'], 
-                'shortcode'=> $sms['OTP_SHORTCODE'], 
-                'mobile'=> $mobilenumber, 
-                'message'=> $message
-            ];
-            curl_setopt($curl, CURLOPT_URL, $sms['OTP_URL'].http_build_query($params));
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-            $curl_response = curl_exec($curl);
-            $response=json_decode($curl_response);
-
             if ($response -> success){
                 $user -> account_pin = $new_pin;
                 $user -> save();
